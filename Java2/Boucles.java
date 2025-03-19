9d87ffbf0962b489b305ab8fd8b4fe73138b6e34
NZOMO RODRIGUE <r.nzomo.tmrcomputing@gmail.com>
2025-03-19 16:19:42 +0100

    Java#8

---
+++ Java2/Boucles.java
@@ -0,0 +1,10 @@
+public class Boucles{
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            
+            System.out.println("Hello World!");
+        }
+    }  // Le corps de la boucle est exécuté 10 fois
+}
